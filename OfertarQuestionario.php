8099b314bb44224b9b5eac1b22c528daf88d1f72
Ezequiel <esbenelli@ucs.br>
2023-04-30 17:08:29 -0300

    tela ofertar questionarios

--- OfertarQuestionario.php
+++ OfertarQuestionario.php
@@ -13,33 +13,37 @@ $daoResp = $factory->getRespondenteDao();
 $respondentes = $daoResp->buscaTodos();
 
 ?>
-<form class="formOfertas" action="CadastraOferta.php" method="POST">
-    <div class="wrapper">
-        <div class="box1">
-            <label for="questionarios">Selecione um questionário:</label>
-            <select name="questionario" id="questionarios">
-                <?php
-                foreach ($questionarios as $questionario) {
-                    echo "<option value=\"" . $questionario->getId() . "\">" . $questionario->getNome() . "</option>";
-                }
-                ?>
-            </select>
-        </div>
-        
-        <div class="box2">
-            <?php
-            foreach ($respondentes as $resp) {
-                echo "<div>";
-                echo "<label>";
-                echo "<input type=\"checkbox\" name=\"respondentesid[]\" value=\"" . $resp->getId() . "\">";
-                echo $resp->getNome() . "</label>";
-                echo "</div>";
-            }
+<div class="formOfertas">
+    <form action="CadastraOferta.php" method="POST">
+        <div class="wrapper">
+            <div class="box1">
+                <label for="questionarios">Selecione um questionário:</label>
+                <select name="questionario" id="questionarios">
+                    <?php
+                    foreach ($questionarios as $questionario) {
+                        echo "<option value=\"" . $questionario->getId() . "\">" . $questionario->getNome() . "</option>";
+                    }
+                    ?>
+                </select>
+            </div>
 
-            ?>
+            <div class="box2">
+                <?php
+                echo "<p style='margin-left: 10px'>Marque os respondentes a serem ofertados:</p>";
+
+                foreach ($respondentes as $resp) {
+                    echo "<div style='margin-left: 10px'>";
+                    echo "<label>";
+                    echo "<input type=\"checkbox\" name=\"respondentesid[]\" value=\"" . $resp->getId() . "\">";
+                    echo $resp->getNome() . "</label>";
+                    echo "</div>";
+                }
+
+                ?>
+            </div>
         </div>
-    </div>
-    <input type="submit" value="Cadastrar" id="btCadastro">
+        <input type="submit" value="Ofertar" id="btCadastro">
+</div>
 </form>
 
 <?php include_once 'LayoutFooter.php' ?>
